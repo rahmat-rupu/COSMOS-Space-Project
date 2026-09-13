9d67da37687abfb79cf3613bd12e40a0acad0963
rahmat-rupu <rahmatrupu75@gmail.com>
2026-09-14 00:51:42 +0600

    mars and saturn info added

--- planets/planet.js
+++ planets/planet.js
@@ -1,5 +1,10 @@
 const home = document.querySelector(".home");
+const saturn = document.querySelector(".saturn-card");
 
 home.addEventListener("click", function () {
   window.location.href = "../index.html";
+});
+
+saturn.addEventListener("click", function () {
+  window.location.href = "../saturn/saturn.html";
 });
